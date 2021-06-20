cdf230ab4e83dba02e1ffd5640ed2743b6785735
ShahinSalimov <mrsalimofff@gmail.com>
2021-06-20 18:21:52 +0400

    bugun hansi gundur?

---
+++ WORKS_/interesting_project/Fruit_picture/html/test2.js
@@ -0,0 +1,4 @@
+var bugun=new Date();
+var gun=bugun.getDay();
+var gunSiyahisi=["birinciGun","ikinciGun","ucuncuGun","dorduncuGun","besincigun","altinciGun","Bazar"]
+document.write("bugun : " + gunSiyahisi[gun] + ".")
