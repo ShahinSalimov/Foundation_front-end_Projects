e190573813d17514043b9b6787e5cb6f8ed5c677
ShahinSalimov <mrsalimofff@gmail.com>
2021-06-21 19:34:35 +0400

    yeni branch

--- WORKS_/my-accordion/ac.js
+++ WORKS_/my-accordion/ac.js
@@ -1,9 +1,10 @@
 var buton=document.getElementsByClassName("ustpanel")
-
+var ishare=document.getElementsByClassName("fa-arrow-circle-down")
 for(var i=0;i<buton.length;i++)
 {
     buton[i].onclick=function(){ 
-     var panel=this.nextElementSibling;   
+     var panel=this.nextElementSibling;
+     this.lastElementChild.classList.toggle("fa-arrow-circle-up")   
        console.log(panel)
        if(panel.style.maxHeight){
            panel.style.maxHeight=null
