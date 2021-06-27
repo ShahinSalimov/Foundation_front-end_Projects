6053409561284e7a55a17b2e1209070eef27354a
ShahinSalimov <mrsalimofff@gmail.com>
2021-06-28 02:29:15 +0400

    js ile factorial ededin tapilmasi

---
+++ WORKS_/My~Exercises/faktorial ededi tap/js/factor.js
@@ -0,0 +1,8 @@
+function  Factorial_Tap(){
+    var eded,i,factorial=1;
+    eded=Number(document.getElementById('F_eded_Yaz').value)
+    for(i=1;i<=eded;i++){
+        factorial*=i
+    }
+    document.getElementById('netice').innerHTML=factorial
+}
