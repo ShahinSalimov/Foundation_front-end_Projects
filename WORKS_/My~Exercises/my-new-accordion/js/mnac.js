a97a781ed191dcde50f01e2045ae8fc7f7fed37f
ShahinSalimov <mrsalimofff@gmail.com>
2021-07-01 20:35:13 +0400

    new accordion completed

--- WORKS_/My~Exercises/my-new-accordion/js/mnac.js
+++ WORKS_/My~Exercises/my-new-accordion/js/mnac.js
@@ -1,14 +1,7 @@
-let accordion = document.getElementsByClassName(".accordion-box");
+const accordion = document.getElementsByClassName('content-box');
 
-for (let i=0; i<acc.length; i++)
-{
-	accordion[i].onclick=function(){
-	this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  };
+for ( i = 0; i < accordion.length; i++ ) {
+	accordion[i].addEventListener('click', function() {
+		this.classList.toggle('active');
+	});
 }
