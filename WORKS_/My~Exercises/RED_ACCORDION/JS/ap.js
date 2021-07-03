6afdbb8be61bdf2cd4103c28797ad31e68bf6815
ShahinSalimov <mrsalimofff@gmail.com>
2021-07-03 16:46:47 +0400

    red accordion yekunlashdi

--- WORKS_/My~Exercises/RED_ACCORDION/JS/ap.js
+++ WORKS_/My~Exercises/RED_ACCORDION/JS/ap.js
@@ -1,16 +1,34 @@
-var acc=document.getElementsByClassName('.main-btn');
+var p01=document.querySelector('.panel01');
+var p02=document.querySelector('.panel02');
+var p03=document.querySelector('.panel03');
 
-var i;
+
 var acar=true;
-for(i=0;i<acc.length;i++){
-    acc[i].onclick=function(){
-        var panel=this.nextElementSibling;
-       if(acar==true){
-           panel.style.diplay="none";
-           acar=false;
-       }else{
-        panel.style.diplay="block";
+
+function func01(){
+    if(acar==true){
+        p01.style.display='none'
+        acar=false;
+    }else{
+        p01.style.display='block'
         acar=true;
-       }
+    }
+}
+function func02(){
+    if(acar==true){
+        p02.style.display='none'
+        acar=false;
+    }else{
+        p02.style.display='block'
+        acar=true;
+    }
+}
+function func03(){
+    if(acar==true){
+        p03.style.display='none'
+        acar=false;
+    }else{
+        p03.style.display='block'
+        acar=true;
     }
 }
