df599681f1e0bd66b362f2c328be23b124399bc4
ShahinSalimov <mrsalimofff@gmail.com>
2021-08-03 16:12:31 +0400

    baki kendleri adli application-min bir hissesi yekunlashdi,with python

---
+++ WORKS_/My~Exercises/BAKI_KENDLERI_with_Python/Baki_kendleri.py
@@ -0,0 +1,39 @@
+Baki_kendleri="""
+                             ******Bakinin-kendleri******
+-----------------------------------------------------------------------------------------------
+1.Maştağa        | 11.Zabrat         | 21.Biləcəri      | 31.Şağan       | 41.Saray
+2.Buzovna        | 12.Balaxanı       | 22.Masazır       | 32.Hövsan      | 42.Sabunçu
+3.Qala           | 13.Keşlə          | 23.Əhmədli       | 33.Dübəndi     | 43.Yeni Suraxanı
+4.Balaxanı       | 14.Binəqədi       | 24.Zağulba       | 34.Hökməli     | 44.Məmmədli
+5.Zığ            | 15.Əmircan        | 25.Qobustan      | 35.Xocahəsən   | 45.Nubar
+6.Binə           | 16.Bilgəh         | 26.Digah         | 36.Lökbatan    
+7.Zirə           | 17.Corat          | 27.Ramana        | 37.Görədil     
+8.Şüvəlan        | 18.Bülbülə        | 28.Pirşağı       | 38.Fatmayı
+9.Türkan         | 19.Güzdək         | 29.Pirallahı     | 39.Nardaran
+10.Kürdəxanı     | 20.Bibiheybət     | 30.Mərdəkan      | 40.Novxanı
+
+------------------------------------------------------------------------------------------------
+"""
+print("zehmet olmazsa haqqinda oyrenmek isteyeceyiniz Baki kendinin sira nomresini daxil edin :")
+secim=int(input("sira_nomresi : "))
+while True:
+    print(Baki_kendleri)
+    if secim==1:
+       Maştağa="""
+       Bakı şəhərinin ən böyük yaşayış məskənlərindən biri hesab olunan, 6 min hektara qədər əraziyə malik  Maştağa kəndi (sonralar qəsəbəyə çevrilib) qədim bir tarixə malikdir.
+       "Maştağa" sözü, bu kəndin özündən əvvəl yaranıb. Görkəmli Azərbaycan tarixçisi Abbasqulu ağa Bakıxanov "Gülüstan-i İrəm" əsərində "Maştağa" toponiminin "massaget" sözündən yaranması fikrini irəli sürmüşdür. Azərbaycan xalqının soykökündə mühüm rol oynayan bu tayfa eramızdan əvvəl VI-V əsrlərdə mövcud olub. Bu toponimin digər mənasını da Abbasqulu ağa Bakıxanov "Gülüstani-İrəm" əsərində "Məşədi ağa" kimi vermişdir.
+       """
+       print(Maştağa)
+       break
+    elif secim==2:
+        Buzovna="""
+         Buzovna kəndinin adı maldarlıqla əlaqədar yaranması və "buzov" mənasını verməsi güman olunur. Lakin bəzi tarixi qaynaqlarda (XIII-XIV əsrlər) bu ərazi "Bozok" tayfalarının adı ilə bağlıdır. Bozok tayfaları qış aylarında bu ərazilərdə məskunlaşmış və təsərrüfat ilə məşğul olurdular. Yay aylarında isə onlar dağlara çəkilirdilər. Buzovna özünün qədim tarixi memarlıq abidələri ilə də zəngindir. Kənddə ən qədim qəbiristanlıq "Hun qəbiristanlığı"dır.
+         Buzovnada həmçinin dörd el “Təhməzov”, “Capayev”, “Hacı Nurəli” və “Məşədi Veysəl” hamamları mövcud olmuşdur.
+        """
+        print(Buzovna)
+        break
+    elif secim==3:
+        Qala="""
+        Qalada ən qədim yaşayış məskəni tunc dövrünə aiddir. İlk tapıntılar kəndin cənub hissəsində əldə edilib. Burada müxtəlif daş tikili qalıqları, saxsı qırıntıları və üzərində rəsmlər olan daşlarla zəngin qədim yaşayış məskəni aşkar edilib. Ümumiyyətlə Qala kəndində və ona bitişik olan ərazidə 215 memarlıq və arxeoloji abidə mövcuddur. Burada e.ə. III minilliyə aid tarixi mühit qorunub saxlanılır. Qala kəndinin ərazisində e.ə III minillikdən XX əsrə qədər tarixi əhəmiyyət daşıyan memarlıq abidələri — 5 məscid, 3 hamam, 4 ovdan, yaşayış evləri, kənd təsərrüfatı təyinatlı binalar, sərdabələr, məqbərələr, kurqanlar, qəsrin qalıqları və s. mövcuddur.
+        """
+        print(Qala) 
