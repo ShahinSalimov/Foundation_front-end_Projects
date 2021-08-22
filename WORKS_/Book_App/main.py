4f2d33d6adaba92fccd80eb6cfd3ee01aab46550
ShahinSalimov <mrsalimofff@gmail.com>
2021-08-22 04:49:17 +0400

    Book_App adli taskin delete hissesi de yekulashdi....90%hazirdir.....

--- WORKS_/Book_App/main.py
+++ WORKS_/Book_App/main.py
@@ -1,36 +1,63 @@
-from flask import Flask,render_template,request,redirect,url_for
-app=Flask(__name__)
+from flask import Flask, render_template, request,redirect
 
-kitablar=[]
+app = Flask(__name__)
+
+Kitablar = []
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/create',methods=['GET','POST'])
+
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    if request.method=='POST':
-        yazar=request.form['muellif']
-        name=request.form['ad']
-        history=request.form['tarix']
-        page=request.form['sehife']
-        title=request.form['xulase']
-        kitab={
-            'muellif':yazar,
-            'ad':name,
-            'tarix':history,
-            'sehife':page,
-            'xulase':title,
+    if request.method == 'POST':
+        ad = request.form['ad']
+        shair = request.form['shair']
+        tarix = request.form['tarix']
+        sehife = request.form['sehife']
+        metn = request.form['metn']
+        kitab = {
+            'id': Kitablar.__len__(),
+            'ad': ad,
+            'shair': shair,
+            'tarix': tarix,
+            'sehife': sehife,
+            'metn': metn
         }
-        kitablar.append(kitab)
+        Kitablar.append(kitab)
+
     return render_template('create.html')
+
 
 @app.route('/show')
 def show():
-    return render_template('show.html',kitablarin_siyahisi=kitablar)
+    return render_template('show.html', Kitablar=Kitablar)
 
-@app.route('/single')
-def single():
-    return render_template('single.html')
+
+@app.route('/single/<id>')
+def single(id):
+    id = int(id)
+    kitab = {}
+
+    for k in Kitablar:
+        if id == k['id']:
+            kitab=k
+        return render_template('single.html', kitab=kitab)
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', Kitablar=Kitablar)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    for kitab in Kitablar:
+        if id == kitab['id']:
+            Kitablar.remove(kitab)
+    return redirect('/admin')
+
 
 
 
